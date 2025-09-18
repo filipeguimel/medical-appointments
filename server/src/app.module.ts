504f09564d6dba45fe82f85570bb4326d5d21dea
Guimel Cavalcante <gfgc@cin.ufpe.br>
2025-09-18 18:00:52 -0300

    feat: implement Appointments CRUD with business rules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Appointment } from './appointments/appointment.entity';
+import { AppointmentsModule } from './appointments/appointments.module';
 
 @Module({
   imports: [
@@ -12,14 +13,15 @@ import { Appointment } from './appointments/appointment.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USERNAME || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'postgres',
+      database: process.env.DATABASE_NAME || 'medical-appointments',
       entities: [Appointment],
       synchronize: process.env.NODE_ENV === 'development',
     }),
+    AppointmentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
